edb2e23f7b2dc38f1998122b21f24b34585df0bb
ppadimala <ppadimala@gmail.com>
2020-09-18 18:12:31 +0530

    Added a method

--- JavaPrograms-master/PallindromePermutaion.java
+++ JavaPrograms-master/PallindromePermutaion.java
@@ -45,4 +45,18 @@ class Sol1 {
 		}
 		return true;
 	}
+	
+	public static int[] getProductArray(int[] arr){
+		if(arr.length ==0 || arr == null){
+			return arr;
+		}
+		int product = 1;
+		for(int i = 0 ; i <arr.length ; i++){
+			product = product * arr[i];
+		}
+		for(int i=0 ; i < arr.length; i++){
+			arr[i] = product/arr[i];
+		}
+		return arr;
+	}
 }
